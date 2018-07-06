f1a95acb486315bff802d163cbbd3b4859b00123
Matteo Esposito <actumatteo@gmail.com>
2018-07-06 00:58:32 -0400

    small updates

--- src/MainMatteo.R
+++ src/MainMatteo.R
@@ -10,15 +10,34 @@
 #   - Set working directories                            #
 #--------------------------------------------------------#
 
+## WD
+setwd("~/Github/DesjardinsDataCup")
 
+## Package loading
+packages <- c("data.table", "rpart", "caret", "gbm", "mgcv", "ggplot2", "dplyr","Hmisc", "xgboost", "corrplot")
+sapply(packages, require, character.only = T)
+
+## Load data
+facturation_train <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/facturation_train.csv")) 
+facturation_test <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/facturation_test.csv")) 
+paiements_train <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/paiements_train.csv")) 
+paiements_test <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/paiements_test.csv")) 
+performance_train <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/performance_train.csv")) 
+performance_test <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/performance_test.csv")) 
+transactions_train <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/transactions_train.csv")) 
+transactions_test <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/transactions_test.csv")) 
+
+sampSol <- as.data.table(read.csv("~/Github/DesjardinsDataCup/Data/sample_solution.csv")) 
 
 #--------------------------------------------------------#
-# 1. Preprocessing                                       #
-# ______________________________________________________ #
-#                                                        #
-#   - detail 1                                           #
-#   - detail 2                                           #
+# 1. Preprocessing                                       
+# ______________________________________________________ 
+#                                                        
+#   - NA/bad formatting checks                           
+#   - Feature engineering
+#   - Table merges
 #--------------------------------------------------------#
+
 
 
 #--------------------------------------------------------#
